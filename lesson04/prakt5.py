8fa93923224e3347aa8dad4d420608055b8f0b40
mmlegenkiy <93904802+mmlegenkiy@users.noreply.github.com>
2023-04-23 18:32:31 +0300

    Start work with prakt5

---
+++ lesson04/prakt5.py
@@ -0,0 +1,9 @@
+# Банкомат видає суму дрібними, але не більше 10 штук кожної дрібної купюри
+
+bills = [50, 100, 200, 500]
+maxval = 10
+money = int(input())
+if money:
+    pass
+else:
+    print("Improper value")
